cb5311db7695e71338a28aae0f488ae7594f56f3
anoopk-extentia <anoop.kumar@extentia.com>
2021-07-29 10:58:24 +0530

    Code improvements.

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ var AWS = require('aws-sdk');
 
 const options = yargs
  .usage("Usage: -f <fn> -b <bucketName> -o <objectName>, -k <key>")
- .option("f", { alias: "fn", describe: "listBuckets, listObjects, getObject", type: "string", demandOption: true })
+ .option("f", { alias: "fn", describe: "listBuckets, listObjects, getObject, removeObject", type: "string", demandOption: true })
  .option("b", { alias: "bucket", describe: "Name of bucket to list objects from", type: "string"})
- .option("o", { alias: "object", describe: "Name of object to get", type: "string"})
+ .option("o", { alias: "object", describe: "Name of object to get or remove", type: "string"})
  .option("k", { alias: "key", describe: "Number of objects to get", type: "string"})
  .argv;
 
@@ -21,44 +21,63 @@ catch{
   return;
 }
 // Create S3 service object
-s3 = new AWS.S3({apiVersion: '2006-03-01'});
+var s3 = new AWS.S3({apiVersion: '2006-03-01'});
 
-if(options.fn == 'listBuckets'){
-    // Call S3 to list the buckets
-    s3.listBuckets(function(err, data) {
-        if (err) {
-        console.log("Error", err);
-        } else {
-        console.log("Success", data.Buckets);
-        }
-    });  
-}
+switch(options.fn){
+  case('listBuckets'):
+      // Call S3 to list the buckets
+      s3.listBuckets(function(err, data) {
+          if (err) {
+          console.log("Error", err);
+          } else {
+          console.log("Success", data.Buckets);
+          }
+      });  
+      break;
 
-if(options.fn == 'listObjects'){
-    var params = {
-        Bucket: options.bucket, 
-        MaxKeys: options.key
-    };
+  var params;
+  case('listObjects'):
+        params = {
+          Bucket: options.bucket, 
+          MaxKeys: options.key
+      };
+      
+      s3.listObjects(params, function(err, data) {
+          if (err) console.log(err, err.stack); // an error occurred
+          else     console.log(data);           // successful response
+        });
+    break;
 
-    
-    s3.listObjects(params, function(err, data) {
-        if (err) console.log(err, err.stack); // an error occurred
-        else     console.log(data);           // successful response
-      });
-}
+  case('getObject'):
+        params = {
+          Bucket: options.bucket, 
+          Key: options.object
+      };
+      
+      s3.getObject(params, function(err, data) {
+          if (err) {
+            console.log("Error", err);
+          } else {
+            let objectData = data.Body.toString('utf-8');
+            console.log("Success", objectData);
+          }
+        });
+  break;
 
-if(options.fn == 'getObject'){
-    var params3 = {
+  case('removeObject'):
+      params = {
         Bucket: options.bucket, 
         Key: options.object
     };
     
-    s3.getObject(params3, function(err, data) {
+    s3.deleteObject(params, function(err, data) {
         if (err) {
           console.log("Error", err);
         } else {
-          let objectData = data.Body.toString('utf-8');
-          console.log("Success", objectData);
+          console.log("Object removed");
         }
       });
+    break;
+  default:
+    console.log("Function", options.fn + " not found. See help with 'npm run help'.");
 }
